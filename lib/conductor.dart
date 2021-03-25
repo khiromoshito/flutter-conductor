cd3a8ef5616bc84a1b5cb50f94071f1a0d640b5a
khiromoshito <66996948+khiromoshito@users.noreply.github.com>
2021-03-25 09:43:44 +0800

    fixed bugs

--- lib/conductor.dart
+++ lib/conductor.dart
@@ -5,6 +5,8 @@ class ConductorConfigs {
   static Conductor globalConductor = new Conductor();
 }
 
+Conductor getGlobalConductor() => ConductorConfigs.globalConductor;
+
 class ConductorListener {
   Function() callout;
   Duration delay;
@@ -13,33 +15,76 @@ class ConductorListener {
   ConductorListener({@required this.callout, this.delay, this.category});
 }
 
-class Conductor {
-  Conductor();
-  Conductor.fromListeners(this.listeners);
+class ConductorValue {
+  dynamic value;
 
-  Map<int, ConductorListener> listeners = {};
-  Map<String, Map<int, ConductorListener>> categories = {};
-  Map<String, dynamic> _values = {};
+  ConductorValue(this.value);
+
+  void update(dynamic value) => this.value = value;
+  void getValue() => this.value;
+}
+
+class Conductor {
+  Conductor([this._values]) {
+    if (this._values == null) this._values = {};
+  }
+
+  Conductor.fromListeners(this.listeners, [this._values]) {
+    if (this._values == null) this._values = {};
+  }
+
+  Conductor.fromOnly(String id, ConductorListener listener, [this._values]) {
+    this.listeners[id] = listener;
+
+    if (listener.category != null) {
+      if (this.categories[listener.category] != null)
+        this.categories[listener.category][id] = listener;
+      else
+        this.categories[listener.category] = {id: listener};
+    }
+
+    if (this._values == null) this._values = {};
+  }
+
+  Map<String, ConductorListener> listeners = {};
+  Map<String, Map<String, ConductorListener>> categories = {};
+  Map<String, ConductorValue> _values = {};
 
   void update(String key, dynamic value) {
-    this._values[key] = value;
+    if (this._values[key] != null) {
+      this._values[key].update(value);
+    } else {
+      this._values.addAll({key: ConductorValue(value)});
+    }
+
     this.broadcast();
   }
 
-  dynamic get(String key) => this._values[key];
+  dynamic get(String key) => this._values[key].getValue();
 
-  void addListener(int id, ConductorListener listener) {
+  void addListener(String id, ConductorListener listener) {
+
     this.listeners[id] = listener;
 
-    if (listener.category != null)
-      this.categories[listener.category][id] = listener;
+    if (listener.category != null) {
+      if (this.categories[listener.category] != null)
+        this.categories[listener.category][id] = listener;
+      else
+        this.categories[listener.category] = {id: listener};
+    }
   }
 
-  Conductor by(String category) =>
-      Conductor.fromListeners(this.categories[category]) ?? {};
+  Conductor by(String category) => Conductor.fromListeners(
+        this.categories[category] ?? {}, this._values);
+  
+
+  Conductor only(String id) => this.listeners[id] != null
+        ? Conductor.fromOnly(id, this.listeners[id], this._values)
+        : Conductor(this._values);
+  
 
   void broadcast() {
-    this.listeners.forEach((int id, ConductorListener listener) =>
+    this.listeners.forEach((String id, ConductorListener listener) =>
         Future.delayed(listener.delay, () => listener.callout()));
   }
 }
@@ -48,25 +93,36 @@ class ConductorBuilder extends StatefulWidget {
   @override
   _ConductorBuilderState createState() => _ConductorBuilderState();
 
-  final int id = ConductorConfigs.idcounter++;
+  final String id;
   final Conductor conductor;
   final Widget Function(BuildContext context, Conductor conductor) builder;
   final String category;
   final Duration delay;
 
   ConductorBuilder(
-      {this.conductor, @required this.builder, this.category, this.delay});
+      {this.conductor,
+      this.id,
+      @required this.builder,
+      this.category,
+      this.delay});
 }
 
 class _ConductorBuilderState extends State<ConductorBuilder> {
   Function() callout = () {};
 
+  String id;
+  Conductor conductor;
+
   @override
   void initState() {
     callout = () => setState(() {});
 
-    (widget.conductor ?? ConductorConfigs.globalConductor).addListener(
-        widget.id,
+    this.id = widget.id ??
+        ("conductorbuilder-" + (ConductorConfigs.idcounter++).toString());
+
+    this.conductor = widget.conductor ?? ConductorConfigs.globalConductor;
+    this.conductor.addListener(
+        this.id,
         ConductorListener(
             callout: callout,
             delay: widget.delay ?? Duration.zero,
@@ -83,6 +139,6 @@ class _ConductorBuilderState extends State<ConductorBuilder> {
 
   @override
   Widget build(BuildContext context) {
-    return widget.builder(context, widget.conductor);
+    return widget.builder(context, this.conductor);
   }
 }
